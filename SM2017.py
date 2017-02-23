388a01df1c1f1b1964d77a69c8c6ed5090dfa762
PaulHancock <paul.hancock@curtin.edu.au>
2017-02-23 17:20:11 +0800

    functions all work with multiple coords at once

--- SM2017.py
+++ SM2017.py
@@ -18,10 +18,13 @@ from scipy.special import gamma
 __author__ = 'Paul Hancock'
 __date__ = '2017-02-23'
 
+seconds_per_year = 3600 * 24 * 365.25
+
 
 class SM(object):
     def __init__(self, ha_file, err_file=None):
         # define some of the constants that we need
+        # i'm saving these here to allow for different instances to have different values
         self.nu = 185e6  # MHz
         self.kpc = kpc.value  # in m
         self.t4 = 0.8  # t/1e4 K
@@ -31,6 +34,8 @@ class SM(object):
         self.c = c.value
         self.beta = 11/3
         self.re = 2.817e-15  # m
+        self.rf_1kpc = np.sqrt(self.c * self.kpc / (2*np.pi*self.nu))  # Fresnel scale assuming that D = 1kpc
+
         self.file = ha_file
         self.err_file = err_file
         self._load_file()
@@ -54,12 +59,12 @@ class SM(object):
         :param position: astropy.coordinates.SkyCoord
         :return:
         """
-        ra = position.ra.degree
-        dec = position.dec.degree
-        x, y = self.wcs.all_world2pix([(ra, dec)], 0)[0]
-        x = np.int(np.floor(x))
-        y = np.int(np.floor(y))
-        iha = self.data[x, y]
+        x, y = zip(*self.wcs.all_world2pix(zip(position.ra.degree, position.dec.degree), 0))
+        x = np.int64(np.floor(x))
+        x = np.clip(x, 0, self.hdu['NAXIS1'])
+        y = np.int64(np.floor(y))
+        y = np.clip(y, 0, self.hdu['NAXIS2'])
+        iha = self.data[y, x]
         return iha
 
     def get_sm(self, position):
@@ -76,23 +81,70 @@ class SM(object):
     def get_xi(self, position):
         """
         calculate the parameter ξ for a given sky coord
-        : param position: astropy.coordinates.SkyCoord
+        :param position: astropy.coordinates.SkyCoord
         :return: parameter ξ
         """
         sm2 = self.get_sm(position)
         rdiff = (2**(2-self.beta) * (np.pi * self.re**2 * (self.c/self.nu)**2 * self.beta) * sm2 * self.kpc *
                  gamma(-self.beta/2)/gamma(self.beta/2))**(1/(2-self.beta))
-        # Fresnel scale assuming that D = 1kpc
-        rf_1kpc = np.sqrt( self.c * self.kpc / (2*np.pi*self.nu))
-        xi = rf_1kpc / rdiff
+        xi = self.rf_1kpc / rdiff
         return xi
 
+    def get_m(self, position):
+        """
+        calculate the modulation index using parameter ξ for a given sky coord
+        :param position: astropy.coordinates.SkyCoord
+        :return:
+        """
+        m = self.get_xi(position)**(-1/3)
+        return m
 
-def test_get_xi():
+    def get_timescale(self, position):
+        """
+        calculate the refractive timescale using parameter ξ for a given sky coord
+        :param position: astropy.coordinates.SkyCoord
+        :return:
+        """
+
+        #TODO: where does this 1e4 come from?
+        tref = self.rf_1kpc * self.get_xi(position) / 1e4 / seconds_per_year
+        return tref
+
+    def get_rms_var(self, position, nyears=1):
+        """
+        calculate the expected RMS variation in nyears at a given sky coord
+        :param position: astropy.coordinates.SkyCoord
+        :param nyears: timescale of interest
+        :return:
+        """
+        t = self.get_m(position)/self.get_timescale(position) * nyears
+        return t
+
+
+def test_all_params():
+    print("Testing with single positions")
     sm = SM(os.path.join('data', 'Halpha_map.fits'), os.path.join('data', 'Halpha_error.fits'))
-    pos = SkyCoord(0, 0, unit=(u.hour, u.degree))
-    print(sm.get_xi(pos))
+    pos = SkyCoord([0], [0], unit=(u.hour, u.degree))
+    print("Hα = {0}".format(sm.get_halpha(pos)))
+    print("ξ = {0}".format(sm.get_xi(pos)))
+    print("m = {0}".format(sm.get_m(pos)))
+    print("sm = {0}".format(sm.get_sm(pos)))
+    print("t0 = {0}".format(sm.get_timescale(pos)))
+    print("rms = {0}".format(sm.get_rms_var(pos)))
+
+
+def test_multi_pos():
+    print("Testing with list of positions")
+    sm = SM(os.path.join('data', 'Halpha_map.fits'), os.path.join('data', 'Halpha_error.fits'))
+    pos = SkyCoord([0, 4, 8, 12, 16, 20]*u.hour, [-90, -45, 0, 45, 90, -26]*u.degree)
+    print("Hα = {0}".format(sm.get_halpha(pos)))
+    print("ξ = {0}".format(sm.get_xi(pos)))
+    print("m = {0}".format(sm.get_m(pos)))
+    print("sm = {0}".format(sm.get_sm(pos)))
+    print("t0 = {0}".format(sm.get_timescale(pos)))
+    print("rms = {0}".format(sm.get_rms_var(pos)))
 
 
 if __name__ == "__main__":
-    test_get_xi()
+    test_all_params()
+    test_multi_pos()
