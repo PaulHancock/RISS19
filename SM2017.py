e02846a82ef5c02231d9c004d0a8871a1fc11a20
PaulHancock <paul.hancock@curtin.edu.au>
2017-02-23 16:48:29 +0800

    move to class based implementation

--- SM2017.py
+++ SM2017.py
@@ -6,72 +6,93 @@ from __future__ import print_function, division
 NE2001 for extragalactic work.
 """
 
+from astropy.constants import kpc, c
+from astropy.coordinates import SkyCoord
 from astropy.io import fits
 from astropy.wcs import WCS
+import astropy.units as u
 import numpy as np
 import os
-from scipy.constants import c
 from scipy.special import gamma
 
 __author__ = 'Paul Hancock'
 __date__ = '2017-02-23'
 
 
-nu = 185e6 #MHz
+class SM(object):
+    def __init__(self, ha_file, err_file=None):
+        # define some of the constants that we need
+        self.nu = 185e6  # MHz
+        self.kpc = kpc.value  # in m
+        self.t4 = 0.8  # t/1e4 K
+        self.lo = 1e18/(self.kpc*1e-3)  # pc
+        self.eps = 1
+        self.D = 1  # kpc
+        self.c = c.value
+        self.beta = 11/3
+        self.re = 2.817e-15  # m
+        self.file = ha_file
+        self.err_file = err_file
+        self._load_file()
 
-def get_halpha(position):
-    """
-    Return the Halpha +\- err for a given location on the sky.
-    :param position:
-    :return:
-    """
+    def _load_file(self):
+        self.hdu = fits.getheader(self.file)
+        self.wcs = WCS(self.hdu)
+        self.data = fits.open(self.file, memmap=True)[0].data
+        if self.err_file:
+            self.err_hdu = fits.getheader(self.err_file)
+            self.err_wcs = WCS(self.err_hdu)
+            self.err_data = fits.open(self.err_file, memmap=True)[0].data
+        else:
+            self.err_hud = self.err_wcs = self.err_data = None
+
+        return
+
+    def get_halpha(self, position):
+        """
+        Return the Halpha for a given location on the sky.
+        :param position: astropy.coordinates.SkyCoord
+        :return:
+        """
+        ra = position.ra.degree
+        dec = position.dec.degree
+        x, y = self.wcs.all_world2pix([(ra, dec)], 0)[0]
+        x = np.int(np.floor(x))
+        y = np.int(np.floor(y))
+        iha = self.data[x, y]
+        return iha
+
+    def get_sm(self, position):
+        """
+        Return the scintillation measure for a given location on the sky.
+        :param position: astropy.coordinates.SkyCoord
+        :return:
+        """
+        iha = self.get_halpha(position)
+        # Cordes2002
+        sm2 = iha/198 * self.t4**0.9 * self.eps**2/(1+self.eps**2) * self.lo**(-2/3)
+        return sm2
+
+    def get_xi(self, position):
+        """
+        calculate the parameter ξ for a given sky coord
+        : param position: astropy.coordinates.SkyCoord
+        :return: parameter ξ
+        """
+        sm2 = self.get_sm(position)
+        rdiff = (2**(2-self.beta) * (np.pi * self.re**2 * (self.c/self.nu)**2 * self.beta) * sm2 * self.kpc *
+                 gamma(-self.beta/2)/gamma(self.beta/2))**(1/(2-self.beta))
+        # Fresnel scale assuming that D = 1kpc
+        rf_1kpc = np.sqrt( self.c * self.kpc / (2*np.pi*self.nu))
+        xi = rf_1kpc / rdiff
+        return xi
 
 
-def get_xi(position):
-    """
-    Calculate the ξ parameter
-    :param posiotin:
-    :return:
-    """
-    hmap = os.path.join('data', 'Halpha_map.fits')
-    iha_wcs = WCS(fits.getheader(hmap))
+def test_get_xi():
+    sm = SM(os.path.join('data', 'Halpha_map.fits'), os.path.join('data', 'Halpha_error.fits'))
+    pos = SkyCoord(0, 0, unit=(u.hour, u.degree))
+    print(sm.get_xi(pos))
 
-    # convert the (ra,dec) position into (x, y) pixel coords)
-    x, y = iha_wcs.all_world2pix([position], 0)[0]
-    x = np.int(np.floor(x))
-    y = np.int(np.floor(y))
-    iha = fits.open(hmap, memmap=True)[0].data[x, y]
-
-    kpc = 3.086e19  # in m
-    t4 = 0.8  # 1e4 K
-    lo = 1e18/(kpc*1e-3) # pc
-    eps = 1
-    D = 1 #kpc
-
-    # Cordes2002
-    sm2 = iha/198 * t4**0.9 * eps**2/(1+eps**2) * lo**(-2/3)
-
-    # Marcquart & Koay 2013
-    # z = 0
-    beta = 11/3
-    re = 2.817e-15  # m
-    # l1ghz = c / 1e9  # wavelength at 1GHz
-    # sm2 * kpc converts SM into SI units.
-    rdiff = (2**(2-beta) * (np.pi * re**2 * (c/nu)**2 * beta) * sm2 * kpc * gamma(-beta/2)/gamma(beta/2))**(1/(2-beta))
-
-
-
-    # Fresnel scale assuming that D = 1kpc
-    rf_1kpc = np.sqrt( c * kpc / (2*np.pi*nu))
-    tref = rf_1kpc**2 / rdiff / 1e4 / 3600 / 24 / 365.25  # years
-
-    xi = rf_1kpc / rdiff
-
-    m = xi**(-1/3.)
-
-    thetaF = rf_1kpc /kpc # rad
-    theta_ref = xi*np.degrees(thetaF)*3600e3 # milli-arcsec
-    return xi
 
 if __name__ == "__main__":
-    print(get_xi((0, 0)))
+    test_get_xi()
