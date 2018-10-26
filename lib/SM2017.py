66616484f2fa2bf72b0b73b27c662fdaf195c8c4
PaulHancock <paul.hancock@curtin.edu.au>
2018-10-26 11:03:40 +0800

    make use of d/v params

--- lib/SM2017.py
+++ lib/SM2017.py
@@ -49,12 +49,12 @@ class SM(object):
         self.t4 = 0.8  # t/1e4 K
         self.lo = 1e18/(self.kpc*1e-3)  # pc
         self.eps = 1
-        self.D = 1  # kpc - distance to the screen
+        self.D = d  # kpc - distance to the screen
         self.c = c.value
         self.beta = 11/3
         self.re = 2.817e-15  # m
         self.rf_1kpc = np.sqrt(self.c * self.kpc / (2*np.pi*self.nu))  # Fresnel scale assuming that D = 1kpc
-        self.v = 1e4  # relative velocity of source/observer in m/s
+        self.v = v  # relative velocity of source/observer in m/s
         #self.log.debug("data:{0} err:{1}".format(ha_file,err_file))
         self.file = ha_file
         self.err_file = err_file
